1acd51bd0111973aa39bea974a2f266edf05a615
Vinay <vinay29841@gmail.com>
2026-08-26 23:34:27 +0530

    Add RQ background worker

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
+import redis
 import secrets
 
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+from rq import Queue
+
 from db import db
 from blocklist import BLOCKLIST
 import models
@@ -13,8 +17,15 @@ from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+
+    connection = redis.from_url(
+    os.getenv("REDIS_URL")
+    ) 
+    app.queue = Queue("emails", connection=connection)
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
--- resources/user.py
+++ resources/user.py
@@ -1,14 +1,21 @@
+import os
+import requests
 from flask.views import MethodView
+from flask import current_app
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, get_jwt, jwt_required, create_refresh_token, get_jwt_identity
+from sqlalchemy import or_
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema,UserRegisterSchema
+from tasks import send_user_registration_email
 from blocklist import BLOCKLIST
 
 blp = Blueprint("Users", "users", description="Operations on users")
+
+
 
 @blp.route("/logout")
 class UserLogout(MethodView):
@@ -16,28 +23,30 @@ class UserLogout(MethodView):
     def post(self):
         jti = get_jwt()["jti"]
         BLOCKLIST.add(jti)
-        return {{"message": "Successfully logged out"}, 200}
+        return {"message": "Successfully logged out"}, 200
 
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exists.")
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+            )
+        ).first():
+            abort(409, message="A user with that username or email already exists.")
         user = UserModel( username = user_data["username"],
+                          email=user_data["email"],
                           password = pbkdf2_sha256.hash(user_data["password"]),   )
         db.session.add(user)
         db.session.commit()
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
 
-        return {"Message" : "User created successfully."}, 201
+        return {"message":"User created successfully"}, 201
+
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
-    """
-    This resource can be useful when testing our Flask app.
-    We may not want to expose it to public users, but for the
-    sake of demonstration in this course, it can be useful
-    when we are manipulating data regarding the users.
-    """
     @blp.response(200, UserSchema)
     def get(self, user_id):
         user = UserModel.query.get_or_404(user_id)
